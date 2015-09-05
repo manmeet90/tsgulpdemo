d367721ec8672018bec501a79df3e273deb64b49
Manmeet Gupta <manmeet.gupta@igate.com>
2015-09-06 00:40:49 +0530

    changes

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express');
 
 var App = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 App.use('/node_modules', express.static(__dirname + '/node_modules'));
 App.use(express.static(__dirname + '/build'));
 App.use(express.static(__dirname));
 
 App.get('/', function(req, res){
-	// res.sendFile(__dirname + "/index.html");
-	res.send("Hello World!");
+	res.sendFile(__dirname + "/index.html");
+	// res.send("Hello World!");
 	// res.end();
 });
 
 App.listen(port, function(){
-	console.log("App started at localhost:3000");
+	console.log("App started at localhost:5000");
 });
